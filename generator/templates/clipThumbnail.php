397534f809500986b9385bb000e25d6b72731cc3
vincent-peugnet <vincent-peugnet@riseup.net>
2024-12-20 00:11:20 +0100

    clip thumbnail placeholder use aspect ratio

--- generator/templates/clipThumbnail.php
+++ generator/templates/clipThumbnail.php
@@ -1,5 +1,5 @@
-<a href="/clip/<?= $id ?>/">
-    <img src="/assets/thumbnail/<?= $id ?>.webp" class="foreground" alt="" loading="lazy">
-    <img src="/assets/thumbnail/<?= $id ?>.gif" class="background" alt="" loading="lazy">
-    <h2>#<?= $id ?></h2>
+<a href="/clip/<?= $clip->id ?>/" style="height: <?= floor(298 / $clip->aspectRatio) ?>px">
+    <img src="/assets/thumbnail/<?= $clip->id ?>.webp" class="foreground" alt="" loading="lazy">
+    <img src="/assets/thumbnail/<?= $clip->id ?>.gif" class="background" alt="" loading="lazy">
+    <h2>#<?= $clip->id ?></h2>
 </a>
--- generator/templates/tag.php
+++ generator/templates/tag.php
@@ -12,7 +12,7 @@
     <ul class="clips">
         <?php foreach ($clips as $clip) : ?>
             <li>
-                <?php $this->insert('clipThumbnail', ['id' => $clip->id]) ?>
+                <?php $this->insert('clipThumbnail', ['clip' => $clip]) ?>
             </li>
         <?php endforeach ?>
     </ul>
