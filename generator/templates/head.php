8edd1237f95755503e02003ce657e3fd8f8e86f9
vincent-peugnet <vincent-peugnet@riseup.net>
2024-12-26 20:56:48 +0100

    add mobile magic line in HTML head layout

--- generator/templates/head.php
+++ generator/templates/head.php
@@ -1,5 +1,6 @@
 <head>
     <title>Carnet - <?= $title ?></title>
+    <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" href="/assets/base.css">
     <?php if ($stylesheet !== null) : ?>
         <link rel="stylesheet" href="/assets/<?= $stylesheet ?>">
