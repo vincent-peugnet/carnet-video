c27900fbffd5aeba214603e0c6d9e8c4a2f89736
vincent-peugnet <vincent-peugnet@riseup.net>
2025-01-03 22:19:02 +0100

    wip try to fix chromium bug using inline-flex li

--- generator/templates/clipIndex.php
+++ generator/templates/clipIndex.php
@@ -7,14 +7,5 @@
 <h1>
     Extraits
 </h1>
-<ul class="clips">
-    <?php foreach ($clips as $clip) : ?>
-        <li>
-            <?php $this->insert('clipThumbnail', ['clip' => $clip]) ?>
-        </li>
-        <?php endforeach ?>
-    </ul>
+<ul class="clips"><?php foreach ($clips as $clip) : ?><li><a href="<?= $basePath ?>/clip/<?= $clip->id ?>/" style="min-height: <?= floor(298 / ($clip->aspectRatio ?? 3)) ?>px; background-color: <?= $clip->color ?? 'grey' ?>;"><img src="<?= $basePath ?>/assets/thumbnail/<?= $clip->id ?>.webp" class="thumbnail" alt="clip id #<?= $clip->id ?>" loading="lazy"><img src="<?= $basePath ?>/assets/preview/<?= $clip->id ?>.webp" class="preview" alt="" loading="lazy"><h2>#<?= $clip->id ?></h2></a></li><?php endforeach ?></ul>
 <?php $this->stop() ?>
-
-
-        
--- generator/templates/clipThumbnail.php
+++ generator/templates/clipThumbnail.php
@@ -1,5 +1,1 @@
-<a href="<?= $basePath ?>/clip/<?= $clip->id ?>/" style="min-height: <?= floor(298 / ($clip->aspectRatio ?? 3)) ?>px; background-color: <?= $clip->color ?? 'grey' ?>;">
-    <img src="<?= $basePath ?>/assets/thumbnail/<?= $clip->id ?>.webp" class="thumbnail" alt="clip id #<?= $clip->id ?>" loading="lazy">
-    <img src="<?= $basePath ?>/assets/preview/<?= $clip->id ?>.webp" class="preview" alt="" loading="lazy">
-    <h2>#<?= $clip->id ?></h2>
-</a>
+<a href="<?= $basePath ?>/clip/<?= $clip->id ?>/" style="min-height: <?= floor(298 / ($clip->aspectRatio ?? 3)) ?>px; background-color: <?= $clip->color ?? 'grey' ?>;"><img src="<?= $basePath ?>/assets/thumbnail/<?= $clip->id ?>.webp" class="thumbnail" alt="clip id #<?= $clip->id ?>" loading="lazy"><img src="<?= $basePath ?>/assets/preview/<?= $clip->id ?>.webp" class="preview" alt="" loading="lazy"><h2>#<?= $clip->id ?></h2></a>
