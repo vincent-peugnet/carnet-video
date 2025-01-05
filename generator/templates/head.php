2e049e61b04202ee2c8aa52cb59447358e80a692
vincent-peugnet <vincent-peugnet@riseup.net>
2025-01-05 02:02:21 +0100

    fix missing basepath for favicon

--- generator/templates/head.php
+++ generator/templates/head.php
@@ -1,7 +1,7 @@
 <head>
     <title>Carnet - <?= $title ?></title>
     <meta name="viewport" content="width=device-width" />
-    <link rel="icon" type="image/png" href="/assets/favicon.png">
+    <link rel="icon" type="image/png" href="<?= $basePath ?>/assets/favicon.png">
     <link rel="stylesheet" href="<?= $basePath ?>/assets/base.css">
     <?php if ($stylesheet !== null) : ?>
         <link rel="stylesheet" href="<?= $basePath ?>/assets/<?= $stylesheet ?>">
