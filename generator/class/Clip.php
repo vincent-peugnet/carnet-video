cbba1883f03f406eb33003ea228767cb860fa498
vincent-peugnet <vincent-peugnet@riseup.net>
2025-06-04 14:06:30 +0200

    use real thumbnail height if available

--- generator/class/Clip.php
+++ generator/class/Clip.php
@@ -2,6 +2,8 @@
 
 namespace Carnet;
 
+use RuntimeException;
+
 class Clip
 {
     public readonly int $id;
@@ -27,5 +29,23 @@ class Clip
         $this->aspectRatio = $json['aspectRatio'] ?? null;
         $this->duration = $json['duration'] ?? null;
         $this->color = $json['color'] ?? null;
+    }
+
+    /**
+     * @throws RuntimeException             If it's impossible to get thumbnail's height
+     */
+    public function getThumbnailHeight(): int
+    {
+        $id = $this->id;
+        $filePath = "build/assets/thumbnail/$id.webp";
+        $imageSize = getimagesize($filePath);
+        if ($imageSize !== false) {
+            $height = $imageSize[1];
+            return $height;
+        } elseif ($this->aspectRatio !== null) {
+            return floor(298 / ($this->aspectRatio ?? 3));
+        } else {
+            throw new RuntimeException('impossible to get thumbnail height');
+        }
     }
 }
--- generator/templates/clipList.php
+++ generator/templates/clipList.php
@@ -5,7 +5,7 @@
 <ul class="clips">
     <?php foreach ($clips as $clip) : ?>
         <li id="<?= $clip->id ?>">
-            <a href="<?= $basePath ?>/clip/<?= $clip->id ?>/" style="min-height: <?= floor(298 / ($clip->aspectRatio ?? 3)) ?>px; background-color: <?= $clip->color ?? 'grey' ?>;">
+            <a href="<?= $basePath ?>/clip/<?= $clip->id ?>/" style="min-height: <?= $clip->getThumbnailHeight() ?>px; background-color: <?= $clip->color ?? 'grey' ?>;">
                 <img src="<?= $basePath ?>/assets/thumbnail/<?= $clip->id ?>.webp" class="thumbnail" alt="clip id #<?= $clip->id ?>" loading="lazy">
                 <img src="<?= $basePath ?>/assets/preview/<?= $clip->id ?>.webp" class="preview" alt="" loading="lazy">
                 <h2>#<?= $clip->id ?></h2>
